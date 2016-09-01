89d4d2b6ad6b6be2b0f43c525012024ba16e96d2
scuhooper <keeling18@marshall.edu>
2016-09-01 09:21:08 -0400

    Assignment 1
    Assignment 1 of class being pushed into repo

---
+++ Assets/Scripts/BasicMovement.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BasicMovement : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if ( Input.GetKey( KeyCode.A ) )
+        {
+            transform.position += new Vector3( -.1f, 0, 0 );
+            Debug.Log( this.name + " is moving left." );
+        }
+        if ( Input.GetKey( KeyCode.D ) )
+        {
+            transform.position += new Vector3( .1f, 0, 0 );
+            Debug.Log( this.name + " is moving right." );
+        }
+        if ( Input.GetKey( KeyCode.W ) )
+        {
+            transform.position += new Vector3( 0, .1f, 0 );
+            Debug.Log( this.name + " is moving up." );
+        }
+        if ( Input.GetKey( KeyCode.S ) )
+        {
+            transform.position += new Vector3( 0, -.1f, 0 );
+            Debug.Log( this.name + " is moving down" );
+        }
+    }
+}
