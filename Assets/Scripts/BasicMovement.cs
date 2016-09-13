460a1776dbcb0eb80351f1d46c75da959b17fc4b
hoop <scuhooper@gmail.com>
2016-09-13 19:33:00 -0400

    Assignment 2 Begin
    Added background to scene.
    Added Collision boxes so player cannot leave camera space.
    Player can now only move vertically on the screen. Added assets for
    laser beam.
    Added Fire function to Player script.
    Created script for Laser.
    Added particle system for ship engines.

--- Assets/Scripts/BasicMovement.cs
+++ Assets/Scripts/BasicMovement.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 
 public class BasicMovement : MonoBehaviour {
+	public GameObject laser;
 
 	// Use this for initialization
 	void Start () {
@@ -10,25 +11,34 @@ public class BasicMovement : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+        //if ( Input.GetKey( KeyCode.A ) )
+        //{
+        //    transform.position += new Vector3( -.1f, 0, 0 );
+        //    Debug.Log( this.name + " is moving left." );
+        //}
+        //if ( Input.GetKey( KeyCode.D ) )
+        //{
+        //    transform.position += new Vector3( .1f, 0, 0 );
+        //    Debug.Log( this.name + " is moving right." );
+        //}
         if ( Input.GetKey( KeyCode.A ) )
         {
-            transform.position += new Vector3( -.1f, 0, 0 );
-            Debug.Log( this.name + " is moving left." );
+            transform.position += new Vector3( 0, .1f, 0 );
+            Debug.Log( name + " is moving up." );
         }
         if ( Input.GetKey( KeyCode.D ) )
         {
-            transform.position += new Vector3( .1f, 0, 0 );
-            Debug.Log( this.name + " is moving right." );
-        }
-        if ( Input.GetKey( KeyCode.W ) )
-        {
-            transform.position += new Vector3( 0, .1f, 0 );
-            Debug.Log( this.name + " is moving up." );
-        }
-        if ( Input.GetKey( KeyCode.S ) )
-        {
             transform.position += new Vector3( 0, -.1f, 0 );
-            Debug.Log( this.name + " is moving down" );
+            Debug.Log( name + " is moving down" );
         }
+		if ( Input.GetMouseButton( 0 ) )
+		{
+			Fire();
+		}
     }
+
+	void Fire()
+	{
+		GameObject newLaser = ( GameObject )Instantiate( laser, transform.position, transform.rotation );
+	}
 }
