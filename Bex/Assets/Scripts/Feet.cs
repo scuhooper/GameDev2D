81d4b65acb5bcfcab479c9b1026cbd6119d7d1d3
scuhooper <keeling18@marshall.edu>
2016-10-02 23:22:11 -0400

    Bex Added enemies
    Added forklift with animation to make lift go up and down.
    Added vending machine as an enemy.
    Added soda cans as projectiles vending machine can shoot.
    Created trigger collider volume for enemy forklift that activates the
    object when the player enters the trigger.

--- Bex/Assets/Scripts/Feet.cs
+++ Bex/Assets/Scripts/Feet.cs
@@ -16,10 +16,7 @@ public class Feet : MonoBehaviour {
 
 	void OnCollisionEnter2D( Collision2D col )
 	{
-		if ( col.gameObject.CompareTag( "Ground" ) )
-		{
-			player.GetComponent<BexPlayer>().SetIsGrounded( true );
-		}
+		player.GetComponent<BexPlayer>().SetIsGrounded( true );
 	}
 
 	void OnCollisionExit2D( Collision2D col )
---
+++ Bex/Assets/Scripts/Forklift.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Forklift : MonoBehaviour {
+	public int health;
+	public int damage;
+
+	bool bIsActive;
+	// Use this for initialization
+	void Start () {
+		bIsActive = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if ( bIsActive)
+			Debug.Log( "Forklift is active!" );
+	}
+
+	/// <summary>
+	/// Just changes the bool of bIsActive
+	/// </summary>
+	void Activate()
+	{
+		bIsActive = !bIsActive;
+	}
+
+	void OnTriggerEnter2D( Collider2D col )
+	{
+		if (col.gameObject.tag == "Player")
+			Activate();
+	}
+
+	void OnTriggerExit2D( Collider2D col )
+	{
+		if ( col.gameObject.tag == "Player" )
+		{
+			Activate();
+			Debug.Log( "Forklift is inactive!" );
+		}
+	}
+}
