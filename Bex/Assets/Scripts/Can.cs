ef9a1dd148edda66c69756ade8eb73de2372c81b
James F Keeling <keeling18@live.marshall.edu>
2016-10-05 14:22:08 -0400

    Bex Can script

---
+++ Bex/Assets/Scripts/Can.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Can : MonoBehaviour {
+    public float speed;
+    public float lifeSpan;
+
+    float timeSpawned;
+
+	// Use this for initialization
+	void Start () {
+        timeSpawned = Time.time;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if ( Time.time > timeSpawned + lifeSpan )
+            Destroy( gameObject );
+        else
+            transform.Translate( new Vector3( -speed * Time.deltaTime, 0, 0 ) );
+	}
+}
