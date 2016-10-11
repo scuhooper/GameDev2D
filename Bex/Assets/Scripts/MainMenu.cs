888669242094895f296c4500ebf739ee773cde56
scuhooper <keeling18@marshall.edu>
2016-10-11 10:34:01 -0400

    Bex UI and Main Menu

---
+++ Bex/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void OnStartButtonClicked()
+	{
+		SceneManager.LoadScene( "Level1" );
+	}
+
+	public void OnControlsButtonClicked()
+	{
+
+	}
+
+	public void OnExitButtonClicked()
+	{
+		Application.Quit();
+	}
+}
